8a742ad514bdef777419cae63a154a7af810c643
DX-MODS <ziyankp44@gmail.com>
2022-12-09 17:26:29 +0530

    Iam Back

---
+++ README.md
@@ -0,0 +1,47 @@
+<h1 align= center>Telegraph-Bot</h1>
+<h3 align = center>Powerful Telegram bot to generate Telegraph links for your media files. This bot can work also in group chats.</h3>
+<p align="center">
+<a href="https://python.org"><img src="http://forthebadge.com/images/badges/made-with-python.svg" alt="made-with-python"></a>
+<br>
+    <img src="https://img.shields.io/github/stars/ZIYAN-SER/Telegraph-Bot?style=for-the-badge&color=yellow" alt="Stars">
+    <img src="https://img.shields.io/github/forks/ZIYAN-SER/Telegraph-Bot?style=for-the-badge&color=green" alt="Forks">
+    <img src="https://img.shields.io/github/watchers/ZIYAN-SER/Telegraph-Bot?style=for-the-badge&color=yellow" alt="Watchers"> <br>
+    <img src="https://img.shields.io/github/license/ZIYAN-SER/Telegraph-Bot?style=for-the-badge&color=green" alt="License">
+    <img src="https://img.shields.io/github/repo-size/ZIYAN-SER/Telegraph-Bot?style=for-the-badge&color=yellow" alt="Repository Size">
+    <img src="https://img.shields.io/github/contributors/ZIYAN-SER/Telegraph-Bot?style=for-the-badge&color=green" alt="Contributors">
+    <img src="https://img.shields.io/github/issues/ZIYAN-SER/Telegraph-Bot?style=for-the-badge&color=yellow" alt="Issues">
+</p>  
+
+## Deployment Methods
+
+### Heroku
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/ZIYAN-SER/Telegraph-Bot)
+
+## Config Vars
+1. `API_ID` : Telegram API_ID, get it from my.telegram.org/apps
+2. `API_HASH` : Telegram API_ID, get it from my.telegram.org/apps
+3. `BOT_TOKEN` : A Valid Telegram Bot Token, get it from @Botfather
+
+## Commands
+
+  `/tl`
+
+- For group chats.
+```
+Simply send a valid media file directly to bot's private chat.
+Valid file types are 'jpeg', 'jpg', 'png', 'mp4' and 'gif'.
+
+To generate links in group chats, add this bot to your group and send the command /tl as a reply to a valid media file.
+```
+## Credits
+- [Telegraph-Bot](https://github.com/ZIYAN-SER)
+- [Pyrogram](https://github.com/pyrogram/pyrogram)
+
+## Warning
+
+- You are free to use this code in any of your projects, but you MUST include the following in your README.md (Copy & paste)
+```
+##Credits
+- [Telegraph Bot By Ziyan] (https://github.com/ZIYAN-SER/Telegraph-Bot)
+```
---
+++ Telegraph-Bot.py
@@ -0,0 +1,73 @@
+#©2022 ZIYAN-SER
+#A PROJECT DONE BY ZIYAN
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from telegraph import upload_file
+import os
+
+ziyanserbot=Client(
+    "Telegraph-Bot",
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"],
+    bot_token = os.environ["BOT_TOKEN"]
+)
+
+@ziyanserbot.on_message(filters.command('start') & filters.private)
+async def start(client, message):
+    text = f"""
+Heya {message.from_user.mention},
+I am here to generate Telegraph links for your media files.
+
+Simply send a valid media file directly to this chat.
+Valid file types are 'jpeg', 'jpg', 'png', 'mp4' and 'gif'.
+
+To generate links in **group chats**, add me to your supergroup and send the command <code>/tl</code> as a reply to a valid media file.
+
+🏠 | [Home](https://t.me/suport_ziyans)
+            """
+    await ziyanserbot.send_message(message.chat.id, text, disable_web_page_preview=True)
+    
+
+@ziyanserbot.on_message(filters.media & filters.private)
+async def get_link_private(client, message):
+    try:
+        text = await message.reply("Processing...")
+        async def progress(current, total):
+            await text.edit_text(f"📥 Downloading media... {current * 100 / total:.1f}%")
+        try:
+            location = f"./media/private/"
+            local_path = await message.download(location, progress=progress)
+            await text.edit_text("📤 Uploading to Telegraph...")
+            upload_path = upload_file(local_path) 
+            await text.edit_text(f"**🌐 | Telegraph Link**:\n\n<code>https://telegra.ph{upload_path[0]}</code>")     
+            os.remove(local_path) 
+        except Exception as e:
+            await text.edit_text(f"**❌ | File upload failed**\n\n<i>**Reason**: {e}</i>")
+            os.remove(local_path) 
+            return                 
+    except Exception:
+        pass        
+
+@ziyanserbot.on_message(filters.command('tl'))
+async def get_link_group(client, message):
+    try:
+        text = await message.reply("Processing...")
+        async def progress(current, total):
+            await text.edit_text(f"📥 Downloading media... {current * 100 / total:.1f}%")
+        try:
+            location = f"./media/group/"
+            local_path = await message.reply_to_message.download(location, progress=progress)
+            await text.edit_text("📤 Uploading to Telegraph...")
+            upload_path = upload_file(local_path) 
+            await text.edit_text(f"**🌐 | Telegraph Link**:\n\n<code>https://telegra.ph{upload_path[0]}</code>")     
+            os.remove(local_path) 
+        except Exception as e:
+            await text.edit_text(f"**❌ | File upload failed**\n\n<i>**Reason**: {e}</i>")
+            os.remove(local_path) 
+            return         
+    except Exception:
+        pass                                           
+
+print("Bot is alive!")
+ziyanserbot.run()
